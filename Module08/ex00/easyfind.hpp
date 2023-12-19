f1db9905485d0699a8696578acffb3a08b03088f
tmarts <tmarts@student.42heilbronn.de>
2023-12-19 20:10:45 +0100

    CPP 08 prepared

---
+++ Module08/ex00/easyfind.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmarts <tmarts@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/19 19:43:36 by tmarts            #+#    #+#             */
+/*   Updated: 2023/12/19 19:47:48 by tmarts           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include "easyfind.tpp"
+
+template <typename T>
+int easyfind(T cont, int i) {
+	
+}
+#endif
---
+++ Module08/ex02/MutantStack.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmarts <tmarts@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/19 19:55:45 by tmarts            #+#    #+#             */
+/*   Updated: 2023/12/19 19:58:49 by tmarts           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+template <typename T>
+class MutantStack: public std::stack {
+	
+};
+
+#endif
